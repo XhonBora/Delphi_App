dea9bf58f2c7cdd7aca0e0e45a9c323e98ecbd93
XhonBora <xhonibora@gmail.com>
2023-11-14 00:28:49 +0100

    Remove unnecessary files and other fixes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -13,30 +13,6 @@ const routes: Routes = [
   // Invoice
   { path: 'invoice/invoice-detail', loadChildren: () => import('./components/pages/invoice/invoice-detail/invoice-detail.module').then(m => m.InvoiceDetailModule), data: { breadcrumb: "Invoice Detail" } },
   { path: 'invoice/invoice-list', loadChildren: () => import('./components/pages/invoice/invoice-list/invoice-list.module').then(m => m.InvoiceListModule), data: { breadcrumb: "Invoice List" } },
-  // Customers
-  { path: 'customer/customer-review', loadChildren: () => import('./components/pages/customer/customer-review/customer-review.module').then(m => m.CustomerReviewModule), data: { breadcrumb: "Customer Review" } },
-  { path: 'customer/customer-list', loadChildren: () => import('./components/pages/customer/customer-list/customer-list.module').then(m => m.CustomerListModule), data: { breadcrumb: "Customer List" } },
-  { path: 'customer/social', loadChildren: () => import('./components/pages/customer/social/social.module').then(m => m.SocialModule), data: { breadcrumb: "Social Activity" } },
-  // Ui-basic
-  { path: 'ui-basic/accordions', loadChildren: () => import('./components/pages/ui-basic/accordions/accordions.module').then(m => m.AccordionsModule), data: { breadcrumb: "Accordions" } },
-  { path: 'ui-basic/alerts', loadChildren: () => import('./components/pages/ui-basic/alerts/alerts.module').then(m => m.AlertsModule), data: { breadcrumb: "Alerts" } },
-  { path: 'ui-basic/buttons', loadChildren: () => import('./components/pages/ui-basic/buttons/buttons.module').then(m => m.ButtonsModule), data: { breadcrumb: "Buttons" } },
-  { path: 'ui-basic/breadcrumbs', loadChildren: () => import('./components/pages/ui-basic/breadcrumbs/breadcrumbs.module').then(m => m.BreadcrumbsModule), data: { breadcrumb: "Breadcrumbs" } },
-  { path: 'ui-basic/badges', loadChildren: () => import('./components/pages/ui-basic/badges/badges.module').then(m => m.BadgesModule), data: { breadcrumb: "Badges" } },
-  { path: 'ui-basic/cards', loadChildren: () => import('./components/pages/ui-basic/cards/cards.module').then(m => m.CardsModule), data: { breadcrumb: "Cards" } },
-  { path: 'ui-basic/progress-bars', loadChildren: () => import('./components/pages/ui-basic/progress-bars/progress-bars.module').then(m => m.ProgressBarsModule), data: { breadcrumb: "Progress Bars" } },
-  { path: 'ui-basic/preloaders', loadChildren: () => import('./components/pages/ui-basic/preloaders/preloaders.module').then(m => m.PreloadersModule), data: { breadcrumb: "Pre-loaders" } },
-  { path: 'ui-basic/pagination', loadChildren: () => import('./components/pages/ui-basic/pagination/pagination.module').then(m => m.PaginationModule), data: { breadcrumb: "Pagination" } },
-  { path: 'ui-basic/tabs', loadChildren: () => import('./components/pages/ui-basic/tabs/tabs.module').then(m => m.TabsModule), data: { breadcrumb: "Tabs" } },
-  { path: 'ui-basic/typography', loadChildren: () => import('./components/pages/ui-basic/typography/typography.module').then(m => m.TypographyModule), data: { breadcrumb: "Typography" } },
-  // Ui-advanced
-  { path: 'ui-advanced/draggables', loadChildren: () => import('./components/pages/ui-advanced/draggables/draggables.module').then(m => m.DraggablesModule), data: { breadcrumb: "Draggables" } },
-  { path: 'ui-advanced/sliders', loadChildren: () => import('./components/pages/ui-advanced/sliders/sliders.module').then(m => m.SlidersModule), data: { breadcrumb: "Sliders" } },
-  { path: 'ui-advanced/modals', loadChildren: () => import('./components/pages/ui-advanced/modals/modals.module').then(m => m.ModalsModule), data: { breadcrumb: "Modals" } },
-  { path: 'ui-advanced/rating', loadChildren: () => import('./components/pages/ui-advanced/rating/rating.module').then(m => m.RatingModule), data: { breadcrumb: "Rating" } },
-  { path: 'ui-advanced/tour', loadChildren: () => import('./components/pages/ui-advanced/tour/tour.module').then(m => m.TourModule), data: { breadcrumb: "Tour" } },
-  { path: 'ui-advanced/cropper', loadChildren: () => import('./components/pages/ui-advanced/cropper/cropper.module').then(m => m.CropperModule), data: { breadcrumb: "Cropper" } },
-  { path: 'ui-advanced/range-slider', loadChildren: () => import('./components/pages/ui-advanced/range-slider/range-slider.module').then(m => m.RangeSliderModule), data: { breadcrumb: "Range Slider" } },
   // Forms
   { path: 'form/form-elements', loadChildren: () => import('./components/pages/form/form-elements/form-elements.module').then(m => m.FormElementsModule), data: { breadcrumb: "Form Elements" } },
   { path: 'form/form-layouts', loadChildren: () => import('./components/pages/form/form-layouts/form-layouts.module').then(m => m.FormLayoutsModule), data: { breadcrumb: "Form Layouts" } },
@@ -48,16 +24,6 @@ const routes: Routes = [
   // Tables
   { path: 'tables/basic-tables', loadChildren: () => import('./components/pages/tables/basic-tables/basic-tables.module').then(m => m.BasicTablesModule), data: { breadcrumb: "Basic Tables" } },
   { path: 'tables/data-tables', loadChildren: () => import('./components/pages/tables/data-tables/data-tables.module').then(m => m.DataTablesModule), data: { breadcrumb: "Data Tables" } },
-  // Popups
-  { path: 'popups/sweet-alerts', loadChildren: () => import('./components/pages/popups/sweet-alerts/sweet-alerts.module').then(m => m.SweetAlertsModule), data: { breadcrumb: "Sweet Alerts" } },
-  { path: 'popups/toast', loadChildren: () => import('./components/pages/popups/toast/toast.module').then(m => m.ToastModule), data: { breadcrumb: "Toast" } },
-  // Icons
-  { path: 'icons/fontawesome', loadChildren: () => import('./components/pages/icons/fontawesome/fontawesome.module').then(m => m.FontawesomeModule), data: { breadcrumb: "Fontawesome Icons" } },
-  { path: 'icons/flaticons', loadChildren: () => import('./components/pages/icons/flaticons/flaticons.module').then(m => m.FlaticonsModule), data: { breadcrumb: "Flaticons" } },
-  { path: 'icons/materialize', loadChildren: () => import('./components/pages/icons/materialize/materialize.module').then(m => m.MaterializeModule), data: { breadcrumb: "Material Icons" } },
-  // Maps
-  { path: 'maps/google-maps', loadChildren: () => import('./components/pages/maps/google-maps/google-maps.module').then(m => m.GoogleMapsModule), data: { breadcrumb: "Google Maps" } },
-  { path: 'maps/vector-maps', loadChildren: () => import('./components/pages/maps/vector-maps/vector-maps.module').then(m => m.VectorMapsModule), data: { breadcrumb: "Vector Maps" } },
   // Dashboard
   { path: 'dashboard/web-analytics', loadChildren: () => import('./components/pages/dashboard/web-analytics/web-analytics.module').then(m => m.WebAnalyticsModule), data: { breadcrumb: "Web Analytics" } },
   { path: 'dashboard/project-management', loadChildren: () => import('./components/pages/dashboard/project-management/project-management.module').then(m => m.ProjectManagementModule), data: { breadcrumb: "Project Management" } },
@@ -78,12 +44,6 @@ const routes: Routes = [
   { path: 'apps/chat', loadChildren: () => import('./components/pages/apps/chat/chat.module').then(m => m.ChatModule), data: { breadcrumb: "Chat" } },
   { path: 'apps/email', loadChildren: () => import('./components/pages/apps/email/email.module').then(m => m.EmailModule), data: { breadcrumb: "Email" } },
   { path: 'apps/to-do-list', loadChildren: () => import('./components/pages/apps/to-do-list/to-do-list.module').then(m => m.ToDoListModule), data: { breadcrumb: "To-Do-List" } },
-  // Additional
-  { path: 'orders', loadChildren: () => import('./components/pages/orders/orders.module').then(m => m.OrdersModule), data: { breadcrumb: "Orders" } },
-  { path: 'restaurants', loadChildren: () => import('./components/pages/restaurants/restaurants.module').then(m => m.RestaurantsModule), data: { breadcrumb: "Restaurants" } },
-  { path: 'sales', loadChildren: () => import('./components/pages/sales/sales.module').then(m => m.SalesModule), data: { breadcrumb: "Sales" } },
-  { path: 'widgets', loadChildren: () => import('./components/pages/widgets/widgets.module').then(m => m.WidgetsModule), data: { breadcrumb: "Widgets" } },
-  { path: 'animation', loadChildren: () => import('./components/pages/animation/animation.module').then(m => m.AnimationModule), data: { breadcrumb: "Animations" } }
 ];
 
 @NgModule({
